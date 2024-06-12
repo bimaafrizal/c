144f59866522a3c61faf0b1ec22c4c503b4a5683
bimaafrizal <bimaafrizalmalna@student.uns.ac.id>
2024-06-12 09:01:52 +0700

    assigment

---
+++ heart-rate.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    //get the current time
+    time_t t;
+    
+    
+    /* code */
+    return 0;
+}
