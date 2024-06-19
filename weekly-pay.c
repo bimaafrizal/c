62262557499cd954e02390dab25f05771c03296f
bimaafrizal <bimaafrizalmalna@student.uns.ac.id>
2024-06-19 10:05:56 +0700

    weekly pay

---
+++ weekly-pay.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+
+    int inputType, total, type;
+    int gaji[3] = {50000, 100000, 125000};
+    bool status = true;
+
+    while (status)
+    {
+        printf("Selamat Datang di Aplikai Perhitungan Gaji");
+        printf("Masukan Type karyawan 1 = Internship, 2 = Kartap, 3 = Kontrak");
+        scanf("%d", &inputType);
+        if()
+    }
+    
+
+    return 0;
+}
+
